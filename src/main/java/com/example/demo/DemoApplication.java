f110041d55bb0d6429d03e9ece3cac260e284697
mahesh-dolas <125395303+mahesh-dolas@users.noreply.github.com>
2026-07-15 23:52:27 +0000

    added DemoApplication file

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,18 +1,20 @@
-package com.example.demo
+package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 public class DemoApplication {
+
     public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class,args);
+        SpringApplication.run(DemoApplication.class, args);
     }
-    
+
     @GetMapping("/")
     public String hello() {
-        return "Hello GitHub Actions"
-    }    
+        return "Hello GitHub Actions";
+    }
 }
